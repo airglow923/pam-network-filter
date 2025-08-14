4adc3b5c85a1396a070f11c7fd171f7f95bbc497
airglow923 <p.hyundeok76@gmail.com>
2025-08-15 04:15:52 +0900

    feat: add placeholder PAM functions

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,24 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+mod pam;
+
+use std::ffi::c_char;
+use std::ffi::c_int;
+
+#[unsafe(no_mangle)]
+pub extern "C" fn pam_sm_authenticate(
+    _pamh: *mut pam::pam_handle_t,
+    _flags: c_int,
+    _argc: c_int,
+    _argv: *const *const c_char,
+) -> c_int {
+    0
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+#[unsafe(no_mangle)]
+pub extern "C" fn pam_sm_setcred(
+    _pamh: *mut pam::pam_handle_t,
+    _flags: c_int,
+    _argc: c_int,
+    _argv: *const *const c_char,
+) -> c_int {
+    0
 }
