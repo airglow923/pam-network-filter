4fc2f3dfb256beece03f3d589c7ffd065583c766
airglow923 <p.hyundeok76@gmail.com>
2025-08-21 23:31:57 +0900

    refactor(test): rename existing file

--- tests/c/src/modstruct-nullcheck.cc
+++ tests/c/src/modstruct-nullcheck.cc
@@ -10,27 +10,27 @@ TEST(PamListfileModstruct, name) {
   EXPECT_STREQ(_pam_listfile_modstruct.name, "pam_network_filter");
 }
 
-TEST(PamListfileModstruct, pam_sm_authenticate) {
+TEST(PamListfileModstruct, pam_sm_authenticate_not_null) {
   EXPECT_NE(_pam_listfile_modstruct.pam_sm_authenticate, nullptr);
 }
 
-TEST(PamListfileModstruct, pam_sm_setcred) {
+TEST(PamListfileModstruct, pam_sm_setcred_not_null) {
   EXPECT_NE(_pam_listfile_modstruct.pam_sm_setcred, nullptr);
 }
 
-TEST(PamListfileModstruct, pam_sm_acct_mgmt) {
+TEST(PamListfileModstruct, pam_sm_acct_mgmt_not_null) {
   EXPECT_NE(_pam_listfile_modstruct.pam_sm_acct_mgmt, nullptr);
 }
 
-TEST(PamListfileModstruct, pam_sm_open_session) {
+TEST(PamListfileModstruct, pam_sm_open_session_not_null) {
   EXPECT_NE(_pam_listfile_modstruct.pam_sm_open_session, nullptr);
 }
 
-TEST(PamListfileModstruct, pam_sm_close_session) {
+TEST(PamListfileModstruct, pam_sm_close_session_not_null) {
   EXPECT_NE(_pam_listfile_modstruct.pam_sm_close_session, nullptr);
 }
 
-TEST(PamListfileModstruct, pam_sm_chauthtok) {
+TEST(PamListfileModstruct, pam_sm_chauthtok_not_null) {
   EXPECT_NE(_pam_listfile_modstruct.pam_sm_chauthtok, nullptr);
 }
 
