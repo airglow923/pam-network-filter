7c7677de8fbb7a0d39097ff585ca854e9e56b646
airglow923 <p.hyundeok76@gmail.com>
2025-08-22 03:37:43 +0900

    test(c): edit test script to be runnable outside test directory test(c): build rust project if target unavailable

--- tests/c/test.sh
+++ tests/c/test.sh
@@ -2,7 +2,20 @@
 
 set -Eeuo pipefail
 
-cmake -B build -DCMAKE_BUILD_TYPE=Debug
+ROOT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
+TARGET_DIR="${ROOT_DIR}/../../target/"
+
+pushd "${ROOT_DIR}"
+
+if [[ ! $(find "${TARGET_DIR}" -type d ! -path "${TARGET_DIR}") ]]; then
+    cargo b
+fi
+
+declare target=$(ls -dt ../../target/* | head -n 1 | xargs basename)
+
+cmake -B build -DCMAKE_BUILD_TYPE="${target}"
 cmake --build build
 cd build
 ctest
+
+popd
