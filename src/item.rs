3ea48d857aa2092214c508b9d80fef1ef892c886
airglow923 <p.hyundeok76@gmail.com>
2025-09-10 14:23:12 +0900

    feat: add more information to error log while getting PAM items refactor: 'pam_get_item_log_err_and_throw' -> 'pam_item_log_err_and_throw' test: add null test case for 'get_pam_connection'

--- src/item.rs
+++ src/item.rs
@@ -19,15 +19,39 @@ fn pam_get_err_msg(val: c_int) -> String {
         pam::PAM_BAD_ITEM => "undefined or inaccessible item".to_owned(),
         pam::PAM_PERM_DENIED => "third argument of pam_get_item is NULL".to_owned(),
         pam::PAM_SYSTEM_ERR => "wrong PAM handle".to_owned(),
-        _ => "".to_owned(),
+        _ => "unknown error".to_owned(),
     };
 }
 
-fn pam_get_item_log_err_and_throw(
+fn pam_item_type_to_string(item_type: c_int) -> String {
+    match item_type {
+        pam::PAM_SERVICE => "service".to_owned(),
+        pam::PAM_USER => "user".to_owned(),
+        pam::PAM_TTY => "tty".to_owned(),
+        pam::PAM_RHOST => "rhost".to_owned(),
+        pam::PAM_CONV => "conv".to_owned(),
+        pam::PAM_AUTHTOK => "authtok".to_owned(),
+        pam::PAM_OLDAUTHTOK => "oldauthtok".to_owned(),
+        pam::PAM_RUSER => "ruser".to_owned(),
+        pam::PAM_USER_PROMPT => "user prompt".to_owned(),
+        pam::PAM_FAIL_DELAY => "fail delay".to_owned(),
+        pam::PAM_XDISPLAY => "xdisplay".to_owned(),
+        pam::PAM_XAUTHDATA => "xauthdata".to_owned(),
+        pam::PAM_AUTHTOK_TYPE => "authtok type".to_owned(),
+        _ => "unknown".to_owned(),
+    }
+}
+
+fn pam_item_log_err_and_throw(
     pamh: *const pam::pam_handle_t,
     val: c_int,
+    item_type: c_int,
 ) -> Result<(), String> {
-    let msg = pam_get_err_msg(val);
+    let msg = format!(
+        "item type: '{}', {}",
+        pam_item_type_to_string(item_type),
+        pam_get_err_msg(val)
+    );
 
     match val {
         pam::PAM_SUCCESS => return Ok(()),
@@ -46,15 +70,15 @@ pub fn get_pam_connection(pamh: *const pam::pam_handle_t) -> Result<Connection, 
     let mut item: *const c_void = std::ptr::null();
 
     let ret = unsafe { pam::pam_get_item(pamh, pam::PAM_SERVICE, &mut item) };
-    pam_get_item_log_err_and_throw(pamh, ret)?;
+    pam_item_log_err_and_throw(pamh, ret, pam::PAM_SERVICE)?;
     let service = parse_c_string(item as *const c_char);
 
     let ret = unsafe { pam::pam_get_item(pamh, pam::PAM_USER, &mut item) };
-    pam_get_item_log_err_and_throw(pamh, ret)?;
+    pam_item_log_err_and_throw(pamh, ret, pam::PAM_USER)?;
     let user = parse_c_string(item as *const c_char);
 
     let ret = unsafe { pam::pam_get_item(pamh, pam::PAM_RUSER, &mut item) };
-    pam_get_item_log_err_and_throw(pamh, ret)?;
+    pam_item_log_err_and_throw(pamh, ret, pam::PAM_RUSER)?;
 
     let ruser = if item.is_null() {
         String::new()
@@ -63,7 +87,7 @@ pub fn get_pam_connection(pamh: *const pam::pam_handle_t) -> Result<Connection, 
     };
 
     let ret = unsafe { pam::pam_get_item(pamh, pam::PAM_RHOST, &mut item) };
-    pam_get_item_log_err_and_throw(pamh, ret)?;
+    pam_item_log_err_and_throw(pamh, ret, pam::PAM_RHOST)?;
 
     let rhost = if item.is_null() {
         String::new()
@@ -86,7 +110,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_get_pam_connection_success() {
+    fn test_get_pam_connection_tp() {
         let conv: pam::pam_conv = pam::pam_conv::default();
         let mut pamh: *mut pam::pam_handle_t = std::ptr::null_mut();
 
@@ -120,5 +144,12 @@ mod tests {
         let ret = unsafe { pam::pam_end(pamh, pam::PAM_SUCCESS) };
 
         assert_eq!(ret, pam::PAM_SUCCESS);
+    }
+
+    #[test]
+    fn test_get_pam_connection_tn_null() {
+        let pamh: *mut pam::pam_handle_t = std::ptr::null_mut();
+        let ret = get_pam_connection(pamh);
+        assert!(ret.is_err());
     }
 }
