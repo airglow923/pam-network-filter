0015088071b4bbeb770f53300dacf3d2127ecb1a
airglow923 <p.hyundeok76@gmail.com>
2025-09-14 00:24:11 +0900

    feat: derive traits for IP related structures feat: stricter subnet pattern for Ipv4Net chore: remove unnecessary comments test(network): add test cases for 'find_ip_match' and 'create_ip_allowlist'

--- src/network.rs
+++ src/network.rs
@@ -1,21 +1,3 @@
-// ip pattern
-// 0.0.0.0
-// 0.0.0.0/0
-// 0.0.0.0-0.0.00
-//
-// 1. remove duplicate checks
-// 2. merge individual ip addresses into subnets and ranges if possible
-//      - check ip is in subnet (easy with library)
-//      - check ip is in range (not directly supported by library)
-//          - iterate through range and collect all in bitset then match
-//              - costly -- iteration is costly
-//          - cut this bs and just compare directly (lower <= ip <= upper)
-//
-//  - store allowlist in three ways:
-//      1. create bitset for individual ip
-//      2. vector of subnets
-//      3. vector of ranges
-
 extern crate ipnet;
 extern crate roaring;
 
@@ -25,6 +7,7 @@ use roaring::RoaringBitmap;
 use std::error::Error;
 use std::net::Ipv4Addr;
 
+#[derive(Debug, Copy, Clone, Eq, PartialEq, Ord, PartialOrd)]
 enum Pattern {
     Ipv4Addr,
     Ipv6Addr,
@@ -34,12 +17,13 @@ enum Pattern {
     Ipv6Range,
 }
 
-enum IpAllowlist {
+#[derive(Debug, Clone)]
+pub enum IpAllowlist {
     V4(Ipv4Allowlist),
 }
 
 #[derive(Debug, Clone)]
-struct Ipv4Allowlist {
+pub struct Ipv4Allowlist {
     ips: RoaringBitmap,
     subnets: Vec<Ipv4Net>,
     ranges: Vec<(Ipv4Addr, Ipv4Addr)>,
@@ -53,7 +37,11 @@ fn find_ip_match(ip: &str) -> Result<Pattern, regex::Error> {
         return Ok(Pattern::Ipv4Range);
     }
 
-    let pattern_ipv4net = Regex::new(format!(r"^{}/\d{{1,2}}$", pattern_ipv4addr_str).as_str())?;
+    // \d       => 0-9
+    // [1-2]?\d => 10-29
+    // 3[0-2]   => 30-32
+    let pattern_ipv4net =
+        Regex::new(format!(r"^{}/(\d|[1-2]?\d|3[0-2])$", pattern_ipv4addr_str).as_str())?;
 
     if pattern_ipv4net.is_match(ip) {
         return Ok(Pattern::Ipv4Net);
@@ -76,7 +64,7 @@ pub fn create_ip_allowlist(ip_list: Vec<String>) -> Result<IpAllowlist, Box<dyn 
     for ip in ip_list {
         let pat = find_ip_match(&ip)?;
 
-        let _: Result<(), Box<dyn Error>> = match pat {
+        let ret: Result<(), Box<dyn Error>> = match pat {
             Pattern::Ipv4Addr => {
                 ips.insert(ip.parse::<Ipv4Addr>()?.to_bits());
                 Ok(())
@@ -96,8 +84,13 @@ pub fn create_ip_allowlist(ip_list: Vec<String>) -> Result<IpAllowlist, Box<dyn 
 
                 Ok(())
             }
-            _ => Err(Box::<dyn Error>::from("")),
+            _ => Err(Box::<dyn Error>::from("no matching pattern")),
         };
+
+        if let Err(e) = ret {
+            println!("asdfasdfasdfasdfasdfasdfasdfasdfasdfasdfok");
+            return Err(e);
+        }
     }
 
     Ok(IpAllowlist::V4(Ipv4Allowlist {
@@ -105,4 +98,109 @@ pub fn create_ip_allowlist(ip_list: Vec<String>) -> Result<IpAllowlist, Box<dyn 
         subnets,
         ranges,
     }))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_find_ip_match_tp_ipv4range() {
+        let ret = find_ip_match("127.0.0.1-127.0.0.1");
+
+        assert!(ret.is_ok());
+        assert_eq!(ret.unwrap(), Pattern::Ipv4Range);
+    }
+
+    #[test]
+    fn test_find_ip_match_tn_ipv4range_preceded_by_invalid() {
+        let ret = find_ip_match("1127.0.0.1-127.0.0.1");
+
+        assert!(ret.is_err());
+        assert_eq_type!(ret.unwrap_err(), regex::Error);
+    }
+
+    #[test]
+    fn test_find_ip_match_tn_ipv4range_superceded_by_invalid() {
+        let ret = find_ip_match("127.0.0.1-127.0.0.1a");
+
+        assert!(ret.is_err());
+        assert_eq_type!(ret.unwrap_err(), regex::Error);
+    }
+
+    #[test]
+    fn test_find_ip_match_tp_ipv4net() {
+        let ret = find_ip_match("127.0.0.1/32");
+
+        assert!(ret.is_ok());
+        assert_eq!(ret.unwrap(), Pattern::Ipv4Net);
+    }
+
+    #[test]
+    fn test_find_ip_match_tp_ipv4net_subnet_0to32() {
+        for i in 0..=32 {
+            let ret = find_ip_match(format!("127.0.0.1/{}", i.to_string()).as_str());
+
+            assert!(ret.is_ok());
+            assert_eq!(ret.unwrap(), Pattern::Ipv4Net);
+        }
+    }
+
+    #[test]
+    fn test_find_ip_match_tn_ipv4net_invalid_subnet() {
+        let ret = find_ip_match("127.0.0.1/33");
+
+        assert!(ret.is_err());
+        assert_eq_type!(ret.unwrap_err(), regex::Error);
+    }
+
+    #[test]
+    fn test_find_ip_match_tn_ipv4net_invalid_subnet_3digits() {
+        let ret = find_ip_match("127.0.0.1/100");
+
+        assert!(ret.is_err());
+        assert_eq_type!(ret.unwrap_err(), regex::Error);
+    }
+
+    #[test]
+    fn test_find_ip_match_tp_ipv4addr() {
+        let ret = find_ip_match("127.0.0.1");
+
+        assert!(ret.is_ok());
+        assert_eq!(ret.unwrap(), Pattern::Ipv4Addr);
+    }
+
+    #[test]
+    fn test_find_ip_match_tn_ipv4addr_preceded_by_invalid() {
+        let ret = find_ip_match("1127.0.0.1");
+
+        assert!(ret.is_err());
+        assert_eq_type!(ret.unwrap_err(), regex::Error);
+    }
+
+    #[test]
+    fn test_find_ip_match_tn_ipv4addr_superceded_by_invalid() {
+        let ret = find_ip_match("127.0.0.1111");
+
+        assert!(ret.is_err());
+        assert_eq_type!(ret.unwrap_err(), regex::Error);
+    }
+
+    // currently regex pattern does not check whether an IP octet is in 0-255
+    // however, this is handled when parsed as Ipv4Addr
+    // doing such check twice is unnecessary, so this behavior is expected
+    #[test]
+    fn test_find_ip_match_fp_ipv4addr_out_of_range() {
+        let ret = find_ip_match("127.0.0.256");
+
+        assert!(ret.is_ok());
+        assert_eq!(ret.unwrap(), Pattern::Ipv4Addr);
+    }
+
+    #[test]
+    fn test_create_ip_allowlist_tn_ipv4addr_out_of_range() {
+        let ret = create_ip_allowlist(vec!["127.0.0.256".to_owned()]);
+
+        assert!(ret.is_err());
+    }
 }
