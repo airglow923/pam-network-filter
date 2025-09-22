e6bdbd5b70ea8a0b6bcddc7be2bc6d36fea8d507
airglow923 <p.hyundeok76@gmail.com>
2025-09-22 23:29:23 +0900

    test: update test script to handle verbose argument

--- test.sh
+++ test.sh
@@ -2,11 +2,33 @@
 
 set -Eeuo pipefail
 
-ROOT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
+declare ROOT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
+declare VERBOSE=""
+
+args=$(getopt -a -o v --long verbose -- "$@")
+
+eval set -- ${args}
+
+while :; do
+    case $1 in
+    -v | --verbose)
+        VERBOSE="--verbose"
+        shift
+        ;;
+    --)
+        shift
+        break
+        ;;
+    *)
+        echo >&2 "Invalid option '$1'"
+        return 1
+        ;;
+    esac
+done
 
 pushd "${ROOT_DIR}"
 
-cargo test
-tests/c/test.sh
+cargo test $VERBOSE
+tests/c/test.sh $VERBOSE
 
 popd
--- tests/c/test.sh
+++ tests/c/test.sh
@@ -2,8 +2,30 @@
 
 set -Eeuo pipefail
 
-ROOT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
-TARGET_DIR="${ROOT_DIR}/../../target/"
+declare ROOT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
+declare TARGET_DIR="${ROOT_DIR}/../../target/"
+declare VERBOSE=""
+
+args=$(getopt -a -o v --long verbose -- "$@")
+
+eval set -- ${args}
+
+while :; do
+    case $1 in
+    -v | --verbose)
+        VERBOSE="--verbose"
+        shift
+        ;;
+    --)
+        shift
+        break
+        ;;
+    *)
+        echo >&2 "Invalid option '$1'"
+        return 1
+        ;;
+    esac
+done
 
 pushd "${ROOT_DIR}"
 
@@ -16,6 +38,6 @@ declare target=$(ls -dt ../../target/* | head -n 1 | xargs basename)
 cmake -B build -DCMAKE_BUILD_TYPE="${target}"
 cmake --build build
 cd build
-ctest
+ctest $VERBOSE
 
 popd
