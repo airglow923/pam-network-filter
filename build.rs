e9798c5f8b59b43ecbe51f84ba86af2e349bd0d4
airglow923 <p.hyundeok76@gmail.com>
2025-09-24 16:47:48 +0900

    refactor(build): simplify syntax and make variable more relevant

--- build.rs
+++ build.rs
@@ -5,26 +5,24 @@ use std::error::Error;
 use std::path::PathBuf;
 
 fn get_binding_for_header(path: &str) -> bindgen::Bindings {
-    let bindings = bindgen::Builder::default()
+    bindgen::Builder::default()
         .header(path)
         .default_macro_constant_type(bindgen::MacroTypeVariation::Signed)
         .derive_default(true)
         .generate_cstr(true)
         .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
         .generate()
-        .expect("Unable to generate bindings");
-
-    bindings
+        .expect("Unable to generate bindings")
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
     println!("cargo:rustc-link-lib=dylib=pam");
 
-    let out_path = PathBuf::from(env::var("OUT_DIR")?);
+    let path_out = PathBuf::from(env::var("OUT_DIR")?);
     let ffi_pam = get_binding_for_header("ffi/pam.h");
 
     ffi_pam
-        .write_to_file(out_path.join("pam.rs"))
+        .write_to_file(path_out.join("pam.rs"))
         .expect("Couldn't write bindings!");
 
     Ok(())
