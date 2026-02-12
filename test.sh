117a439fe269b4532af96845a60ab7c4f7890d00
airglow923 <p.hyundeok76@gmail.com>
2026-02-12 16:46:40 +0900

    test: run IPv6 tests only if supported

--- test.sh
+++ test.sh
@@ -3,7 +3,9 @@
 set -Eeuo pipefail
 
 declare ROOT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
+declare FILE_OPT_IPV6="/proc/sys/net/ipv6/conf/all/disable_ipv6"
 declare VERBOSE=""
+declare ARGS=""
 
 args=$(getopt -a -o v --long verbose -- "$@")
 
@@ -28,7 +30,12 @@ done
 
 pushd "${ROOT_DIR}"
 
-cargo test $VERBOSE
+# skip IPv6 tests if not supported or disabled
+if [ ! -f "$FILE_OPT_IPV6" ] || [ $(cat "$FILE_OPT_IPV6") == "1" ]; then
+    ARGS="${ARGS} --skip ipv6"
+fi
+
+cargo test $VERBOSE -- $ARGS
 tests/c/test.sh $VERBOSE
 
 popd
